837751521c8fe4680522589f392a7581dc4db266
Myles Henderson <myles@minware.com>
2025-06-06 14:12:46 -0400

    update

--- src/target_s3_json/s3.py
+++ src/target_s3_json/s3.py
@@ -304,15 +304,18 @@ def main(lines: TextIO = sys.stdin) -> None:
             Loader(config | {'client': client, 'executor': executor, 'add_metadata_columns': True }, writeline=save_s3).run(curLines)
         if not curLines.stoppedState():
             break
+    
+    # Parse the path template to get components
+    components = parse_path_template(config['path_template'])
+    
+    # Run cleanup using the parsed components
+    # Remove this after the process has executed for all active ingests
+    # TODO: https://minware.atlassian.net/browse/MW-6495
+    try:
+        cleanup_empty_jsonl_files(bucket=config['s3_bucket'], client=client, path_components=components)
+    except Exception as e:
+        LOGGER.error(f"Failed to cleanup empty JSONL files: {str(e)}")
 
-        # Parse the path template to get components
-        components = parse_path_template(config['path_template'])
-        
-        # Run cleanup using the parsed components
-        try:
-            cleanup_empty_jsonl_files(bucket=config['s3_bucket'], client=client, path_components=components)
-        except Exception as e:
-            LOGGER.error(f"Failed to cleanup empty JSONL files: {str(e)}")
     # After processing is complete, create and refresh the Snowflake stage
     stage = None
     try:
